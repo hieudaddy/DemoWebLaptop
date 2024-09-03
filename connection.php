6988ba354db160c0b576ec8cb135ad7c5190ed32
hieudaddy <93580415+hieudaddy@users.noreply.github.com>
2024-09-03 13:39:50 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,3 +1,10 @@
 <?php
-    $conn = new PDO("dbhades.mysql.database.azure.com","lehieu","Hieztlyn23","web_laptop");
+$mysqli = new mysqli("dbhades.mysql.database.azure.com","lehieu","Hieztlyn23","web_laptop");
+$mysqli->set_charset("utf8");
+
+// Check connection
+if ($mysqli -> connect_errno) {
+  echo "Kết nối MYSQLi lỗi: " . $mysqli -> connect_error;
+  exit();
+}
 ?>
